28099121d141eabcbc50310af5b9ff7358fae588
erfanpaya2021 <erfanpaya2021@gmail.com>
2021-12-04 21:51:00 +0330

    add clock text section

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CIRCLE CLOCK SCRIPTS
 const hour = document.getElementById("clockHour"),
     minutes = document.getElementById("clockMinutes"),
     seconds = document.getElementById("clockSeconds");
@@ -15,3 +16,64 @@ const clock = () => {
 };
 
 setInterval(clock, 1000);
+
+// TEXT CLOCK SCRIPTS
+const textHour = document.getElementById("textHour"),
+    textMinutes = document.getElementById("textMinutes"),
+    textAmPm = document.getElementById("textAmPm"),
+    textDay = document.getElementById("textDay"),
+    textMonth = document.getElementById("textMonth"),
+    textYear = document.getElementById("textYear");
+
+const clockText = () => {
+    let date = new Date();
+    let hh = date.getHours(),
+        mm = date.getMinutes(),
+        ampm,
+        day = date.getDate(),
+        month = date.getMonth(),
+        year = date.getFullYear();
+
+    // HOUR CONFIGS
+    if (hh >= 12) {
+        hh = hh - 12;
+        ampm = "PM";
+    } else {
+        ampm = "AM";
+    }
+
+    if (hh == 0) {
+        hh = 12;
+    }
+
+    hh = hh < 10 ? `0${hh}` : hh;
+
+    // MINUTES CONFIG
+    mm = mm < 10 ? `0${mm}` : mm;
+
+    // SHOW HH,MM,AMPM
+    textHour.innerText = `${hh}:`;
+    textMinutes.innerText = mm;
+    textAmPm.innerText = ampm;
+
+    const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+    textDay.innerText = day;
+    textMonth.innerText = months[month];
+    console.log(month);
+    textYear.innerText = year;
+};
+
+setInterval(clockText, 1000);
